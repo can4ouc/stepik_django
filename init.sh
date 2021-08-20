d7ce8ae51458606628c643527af52bf07be6405e
aleksandr.orlov <aleksandr.orlov@semrush.com>
2021-08-20 22:39:24 +0300

    update config

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
+sudo sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+gunicorn --bind='0.0.0.0:8080' web.hello:app
