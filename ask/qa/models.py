2086b9a700ca2f0e93c9dcad915aab34a0e8119c
can4ouc <can4ouc@yandex.ru>
2021-09-01 00:38:28 +0300

    delete manager

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,17 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
-class QuestionManager(models.Manager):
-    def new(self):
-        return self.order_by('-added_at')
-
-    def popular(self):
-        return self.order_by('-rating')
-
-
 class Question(models.Model):
-    objects = QuestionManager()
     title = models.CharField(max_length=255, null=False)
     text = models.TextField()
     added_at = models.DateField(auto_now_add=True)
