13792773d2428842652ac651237f044cf317113e
aleksandr.orlov <aleksandr.orlov@semrush.com>
2021-09-04 02:46:35 +0300

    fix

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,14 +2,14 @@ from django.conf.urls import url
 from django.http import HttpResponse
 
 from . import views
-from .views import question, popular, index
+from .views import question, popular, index, ask
 
 urlpatterns = [
     url(r'^$', index, name='index'),
     # url(r'^login/$', views.test, name='login'),
     # url(r'^signup/$', views.test, name='signup'),
     url(r'^question/(?P<pk>\d+)/', question, name='question'),
-    url(r'^ask/$', views.test, name='ask'),
+    url(r'^ask/$', ask, name='ask'),
     url(r'^popular/$', popular, name='popular'),
     # url(r'^new/$', views.test, name='new'),
     #url(r'^(?P<num>\d+)/$', question),
