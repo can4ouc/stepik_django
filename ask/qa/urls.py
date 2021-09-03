cc2dea1c42a696832fb18ae5af1bed17f7d3b793
aleksandr.orlov <aleksandr.orlov@semrush.com>
2021-09-04 02:39:13 +0300

    add forms

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # url(r'^login/$', views.test, name='login'),
     # url(r'^signup/$', views.test, name='signup'),
     url(r'^question/(?P<pk>\d+)/', question, name='question'),
-    # url(r'^ask/$', views.test, name='ask'),
+    url(r'^ask/$', views.test, name='ask'),
     url(r'^popular/$', popular, name='popular'),
     # url(r'^new/$', views.test, name='new'),
     #url(r'^(?P<num>\d+)/$', question),
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,20 +1,32 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 # Create your views here.
 from django.template import loader
 
+from .forms import AskForm, AnswerForm
 from .models import Question
 
 
 def question(request, num, ):
     try:
-        q = Question.objects.get(pk=num)
+        q = Question.objects.get(id=num)
     except Question.DoesNotExist:
         raise Http404
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            form._user = request.user
+            _ = form.save()
+            url = q.get_url()
+            return HttpResponseRedirect(url)
+    else:
+        form = AnswerForm(initial={'question': q.id})
 
-    return render(request, 'question.html', {'question': q, })
+    return render(request, 'question.html', {'question': q,
+                                             'form': form,
+                                             'user': request.user,})
 
 
 def index(request):
@@ -51,5 +63,20 @@ def popular(request):
                    'paginator': paginator,
                    'questions': page.object_list,
                    'page': page, })
+
+
+def ask(request):
+    if request.method == "POST":
+        form = AskForm(request.POST)
+        if form.is_valid():
+            form._user = request.user
+            post = form.save()
+            url = post.get_url()
+            return HttpResponseRedirect(url)
+    else:
+        form = AskForm()
+    return render(request, 'ask.html', {'form': form,
+                                        'user': request.user,
+                                        })
 
 
